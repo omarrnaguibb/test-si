c6009fe9f67441251872614de665d4782e1f7df4
omarrnaguibb <130666749+omarrnaguibb@users.noreply.github.com>
2026-08-20 13:58:33 +0300

    Edit

--- src/Pages/ThirdPartyOffers.jsx
+++ src/Pages/ThirdPartyOffers.jsx
@@ -21,7 +21,7 @@ const ThirdPartyOffers = ({ loading, setLoading }) => {
 
   const offers = [
     {
-      company: 'الصقر للتأمين', logo: '/index_files/Sagr.svg', price: '376.66', rating: 4,
+      company: 'الصقر للتأمين', logo: '/index_files/Sagr.svg', price: '277.00', rating: 4,
       features: [{text: 'المسؤولية المدنية تجاه الغير بحد أقصى 10,000,000 ريال', price: 'مشمول'}]
     },
     {
